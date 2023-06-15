6a44d1c5b61296f8ee9573dfaded3b3eee8d6089
テイ　キタク <teikitaku.dat@gmail.com>
2023-06-15 13:38:29 +0900

    bingo!ゲーム

--- fukuv0615/Form1.cs
+++ fukuv0615/Form1.cs
@@ -2,9 +2,35 @@ namespace fukuv0615
 {
     public partial class Form1 : Form
     {
+
+        static Random rand = new Random();
+        int ans = rand.Next(100);
+        int count = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            count++;
+            int guess = int.Parse(textBox1.Text);
+            if (guess==ans)
+            {
+                MessageBox.Show("Bingo!");
+                count = 0;
+                ans = rand.Next(100);   
+            }
+            else if(guess < ans)
+                MessageBox.Show("Less");
+            else
+                MessageBox.Show("Greater");
         }
     }
 }
